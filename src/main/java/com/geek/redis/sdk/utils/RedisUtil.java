e1a212cac647b5b24d0e17ce0afbfcc674253e28
wanggang2632@qianmi.com <wanggang2632@qianmi.com>
2019-01-22 17:39:14 +0800

    根据redis命令集合整理对应的RedisTemplate函数集合

--- src/main/java/com/geek/redis/sdk/utils/RedisUtil.java
+++ src/main/java/com/geek/redis/sdk/utils/RedisUtil.java
@@ -1,9 +1,15 @@
 package com.geek.redis.sdk.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.DataType;
+import org.springframework.data.redis.core.BulkMapper;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.query.SortQuery;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.lang.Nullable;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -11,6 +17,7 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Redis 工具类
+ * Redis 命令参考 对应 Java RedisTemplate API
  * @author: wanggang
  * @createDate: 2019/1/17 11:37
  * @version: 1.0
@@ -20,62 +27,339 @@ public final class RedisUtil {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    //=============================common============================
+    //=============================RedisTemplate针对Key操作============================
+
     /**
-     * 指定缓存失效时间
+     * DEL key
+     * 功能描述：删除给定的一个key。不存在的 key 会被忽略。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：
+     *          O(N)， N 为被删除的 key 的数量。
+     *          删除单个字符串类型的 key ，时间复杂度为O(1)。
+     *          删除单个列表、集合、有序集合或哈希表类型的 key ，时间复杂度为O(M)， M 为以上数据结构内的元素数量。
      * @param key 键
-     * @param time 时间(秒)
-     * @return
+     * @return True:删除成功，false:删除失败
      */
-    public boolean expire(String key,long time){
-        try {
-            if(time>0){
-                redisTemplate.expire(key, time, TimeUnit.SECONDS);
-            }
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
+    public Boolean delByKey(String key){
+       return redisTemplate.delete(key);
     }
 
     /**
-     * 根据key 获取过期时间
-     * @param key 键 不能为null
-     * @return 时间(秒) 返回0代表为永久有效
+     * DEL [key ...]
+     * 功能描述：删除给定的一个key。不存在的 key 会被忽略。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：
+     *          O(N)， N 为被删除的 key 的数量。
+     *          删除单个字符串类型的 key ，时间复杂度为O(1)。
+     *          删除单个列表、集合、有序集合或哈希表类型的 key ，时间复杂度为O(M)， M 为以上数据结构内的元素数量。
+     * @param keys 多个键
+     * @return 被删除 key 的数量。
      */
-    public long getExpire(String key){
+    public Long delByKey(String ... keys){
+       return redisTemplate.delete(CollectionUtils.arrayToList(keys));
+    }
+
+    /**
+     * DUMP key
+     * 功能描述：序列化给定 key ，并返回被序列化的值，使用 RESTORE 命令可以将这个值反序列化为 Redis 键。
+     *              序列化生成的值有以下几个特点：
+     *              它带有 64 位的校验和，用于检测错误， RESTORE 在进行反序列化之前会先检查校验和。
+     *              值的编码格式和 RDB 文件保持一致。
+     *              RDB 版本会被编码在序列化值当中，如果因为 Redis 的版本不同造成 RDB 格式不兼容，那么 Redis 会拒绝对这个值进行反序列化操作。
+     *              序列化的值不包括任何生存时间信息。
+     * 可用版本：>= 2.6.0
+     * 时间复杂度：
+     *          查找给定键的复杂度为 O(1) ，对键进行序列化的复杂度为 O(N*M) ，其中 N 是构成 key 的 Redis 对象的数量，而 M 则是这些对象的平均大小。
+     *          如果序列化的对象是比较小的字符串，那么复杂度为 O(1) 。
+     * @param key
+     * @return 如果 key 不存在，那么返回 nil 。否则，返回序列化之后的值。
+     */
+    public byte[] dumpByKey(String  key){
+       return redisTemplate.dump(key);
+    }
+
+    /**
+     * EXISTS key
+     * 功能描述：检查给定 key 是否存在。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：O(1)
+     * @param key 键
+     * @return true 存在 false不存在
+     */
+    public boolean existsByKey(String key){
+        return redisTemplate.hasKey(key);
+    }
+
+    /**
+     * EXPIRE key seconds
+     * 功能描述：为给定 key 设置生存时间，当 key 过期时(生存时间为 0 )，它会被自动删除。在 Redis 中，带有生存时间的 key 被称为『易失的』(volatile)。
+     *          生存时间可以通过使用 DEL 命令来删除整个 key 来移除，或者被 SET 和 GETSET 命令覆写(overwrite)，这意味着，如果一个命令只是修改(alter)一个带生存时间的 key 的值而不是用一个新的 key 值来代替(replace)它的话，
+     *          那么生存时间不会被改变。比如说，对一个 key 执行 INCR 命令，对一个列表进行 LPUSH 命令，或者对一个哈希表执行 HSET 命令，这类操作都不会修改 key 本身的生存时间。
+     *          另一方面，如果使用 RENAME 对一个 key 进行改名，那么改名后的 key 的生存时间和改名前一样。
+     *          RENAME 命令的另一种可能是，尝试将一个带生存时间的 key 改名成另一个带生存时间的 another_key ，这时旧的 another_key (以及它的生存时间)会被删除，然后旧的 key 会改名为 another_key ，因此，新的 another_key 的生存时间也和原本的 key 一样。
+     *          使用 PERSIST 命令可以在不删除 key 的情况下，移除 key 的生存时间，让 key 重新成为一个『持久的』(persistent) key 。
+     * 更新生存时间：
+     *          可以对一个已经带有生存时间的 key 执行 EXPIRE 命令，新指定的生存时间会取代旧的生存时间。
+     * 过期时间的精确度：
+     *          在 Redis 2.4 版本中，过期时间的延迟在 1 秒钟之内 —— 也即是，就算 key 已经过期，但它还是可能在过期之后一秒钟之内被访问到，而在新的 Redis 2.6 版本中，延迟被降低到 1 毫秒之内
+     * Redis 2.1.3 之前的不同之处：
+     *          在 Redis 2.1.3 之前的版本中，修改一个带有生存时间的 key 会导致整个 key 被删除，这一行为是受当时复制(replication)层的限制而作出的，现在这一限制已经被修复。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：O(1)
+     * @param key 键
+     * @param time 时间(秒)
+     * @return 设置成功返回 true 。当 key 不存在或者不能为 key 设置生存时间时(比如在低于 2.1.3 版本的 Redis 中你尝试更新 key 的生存时间)，返回 false 。
+     */
+    public boolean expireByKey(String key,long time){
+       return redisTemplate.expire(key, time, TimeUnit.SECONDS);
+    }
+
+    /**
+     * EXPIREAT key timestamp
+     * 功能描述：EXPIREAT 的作用和 EXPIRE 类似，都用于为 key 设置生存时间。不同在于 EXPIREAT 命令接受的时间参数是 UNIX 时间戳(unix timestamp)。
+     * 可用版本：>= 1.2.0
+     * 时间复杂度：O(1)
+     * @param key
+     * @param date
+     * @return 如果生存时间设置成功，返回 true 。当 key 不存在或没办法设置生存时间，返回 false 。
+     */
+    public Boolean expireAtByKey(String key, Date date){
+        return  redisTemplate.expireAt(key,date);
+    }
+
+    /**
+     * KEYS pattern
+     * 功能描述：查找所有符合给定模式 pattern 的 key 。
+     *          KEYS * 匹配数据库中所有 key 。
+     *          KEYS h?llo 匹配 hello ， hallo 和 hxllo 等。
+     *          KEYS h*llo 匹配 hllo 和 heeeeello 等。
+     *          KEYS h[ae]llo 匹配 hello 和 hallo ，但不匹配 hillo 。
+     *          特殊符号用 \ 隔开
+     *          KEYS 的速度非常快，但在一个大的数据库中使用它仍然可能造成性能问题，如果你需要从一个数据集中查找特定的 key ，你最好还是用 Redis 的集合结构(set)来代替。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：O(N)， N 为数据库中 key 的数量。
+     * @param pattern
+     * @return 符合给定模式的 key 列表。
+     */
+    public Set<String> keysByKey(String pattern){
+        return  redisTemplate.keys(pattern);
+    }
+
+    //MIGRATE暂时未找到对应的方法
+
+    /**
+     * MOVE key db
+     * 功能描述：将当前数据库的 key 移动到给定的数据库 db 当中。
+     *           如果当前数据库(源数据库)和给定数据库(目标数据库)有相同名字的给定 key ，或者 key 不存在于当前数据库，那么 MOVE 没有任何效果。
+     *           因此，也可以利用这一特性，将 MOVE 当作锁(locking)原语(primitive)。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：O(1)
+     * @param key
+     * @param dbIndex
+     * @return 移动成功返回 true ，失败则返回 false
+     */
+    public Boolean moveByKey(String key, int dbIndex){
+        return  redisTemplate.move(key,dbIndex);
+    }
+
+    //OBJECT暂时未找到对应的方法
+
+    /**
+     * PERSIST key
+     * 功能描述：移除给定 key 的生存时间，将这个 key 从『易失的』(带生存时间 key )转换成『持久的』(一个不带生存时间、永不过期的 key )。
+     * 可用版本：>= 2.2.0
+     * 时间复杂度：O(1)
+     * @param key
+     * @return 当生存时间移除成功时，返回 true .如果 key 不存在或 key 没有设置生存时间，返回 false 。
+     */
+    public Boolean persistByKey(String key) {
+        return  redisTemplate.persist(key);
+    }
+
+    /**
+     * PEXPIRE key milliseconds
+     * 功能描述：这个命令和 EXPIRE 命令的作用类似，但是它以毫秒为单位设置 key 的生存时间，而不像 EXPIRE 命令那样，以秒为单位。
+     * 可用版本：>= 2.6.0
+     * 时间复杂度：O(1)
+     * @param key
+     * @param time
+     * @return 设置成功，返回 true ，key 不存在或设置失败，返回 false
+     */
+    public boolean pexpireByKey(String key,long time){
+        return redisTemplate.expire(key, time, TimeUnit.MICROSECONDS);
+    }
+
+    /**
+     * PEXPIREAT key milliseconds-timestamp
+     * 功能描述：这个命令和 EXPIREAT 命令类似，但它以毫秒为单位设置 key 的过期 unix 时间戳，而不是像 EXPIREAT 那样，以秒为单位。
+     * 可用版本：>= 2.6.0
+     * 时间复杂度：O(1)
+     * @param key
+     * @param date
+     * @return 如果生存时间设置成功，返回 true 。当 key 不存在或没办法设置生存时间时，返回 false 。(查看 EXPIRE 命令获取更多信息)
+     */
+    public Boolean pexpireAtByKey(String key, Date date){
+        return  redisTemplate.expireAt(key,date);
+    }
+
+
+    /**
+     * PTTL key
+     * 功能描述：这个命令类似于 TTL 命令，但它以毫秒为单位返回 key 的剩余生存时间，而不是像 TTL 命令那样，以秒为单位。
+     *           在 Redis 2.8 以前，当 key 不存在，或者 key 没有设置剩余生存时间时，命令都返回 -1 。
+     * 可用版本：>= 2.6.0
+     * 时间复杂度：O(1)
+     * @param key 键 不能为null
+     * @return  当 key 不存在时，返回 -2 。当 key 存在但没有设置剩余生存时间时，返回 -1 。否则，以毫秒为单位，返回 key 的剩余生存时间。
+     */
+    public long pttlByKey(String key){
+        return redisTemplate.getExpire(key,TimeUnit.MICROSECONDS);
+    }
+
+
+    /**
+     * RANDOMKEY
+     * 功能描述：从当前数据库中随机返回(不删除)一个 key 。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：O(1)
+     * @return 当数据库不为空时，返回一个 key 。当数据库为空时，返回 nil 。
+     */
+    public Object randomKeyByKey() {
+        return redisTemplate.randomKey();
+    }
+
+    /**
+     * RENAME key newkey
+     * 功能描述：将 key 改名为 newkey 。
+     *           当 key 和 newkey 相同，或者 key 不存在时，返回一个错误。
+     *           当 newkey 已经存在时， RENAME 命令将覆盖旧值。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：O(1)
+     * @param oldKey
+     * @param newKey
+     */
+    public void renameByKey(String oldKey, String newKey) {
+         redisTemplate.rename(oldKey,newKey);
+    }
+
+    /**
+     * RENAMENX key newkey
+     * 功能描述：当且仅当 newkey 不存在时，将 key 改名为 newkey 。当 key 不存在时，返回一个错误。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：O(1)
+     * @param oldKey
+     * @param newKey
+     * @return 修改成功时，返回 true 。如果 newkey 已经存在，返回 false 。
+     */
+    public Boolean renamenxByKey(String oldKey, String newKey) {
+       return redisTemplate.renameIfAbsent(oldKey,newKey);
+    }
+
+    /**
+     * RESTORE key ttl serialized-value
+     * 功能描述：反序列化给定的序列化值，并将它和给定的 key 关联。
+     *           参数 ttl 以毫秒为单位为 key 设置生存时间；如果 ttl 为 0 ，那么不设置生存时间。
+     *           RESTORE 在执行反序列化之前会先对序列化值的 RDB 版本和数据校验和进行检查，如果 RDB 版本不相同或者数据不完整的话，那么 RESTORE 会拒绝进行反序列化，并返回一个错误。
+     * 可用版本：>= 2.6.0
+     * 时间复杂度：查找给定键的复杂度为 O(1) ，对键进行反序列化的复杂度为 O(N*M) ，其中 N 是构成 key 的 Redis 对象的数量，而 M 则是这些对象的平均大小。
+     *             有序集合(sorted set)的反序列化复杂度为 O(N*M*log(N)) ，因为有序集合每次插入的复杂度为 O(log(N)) 。如果反序列化的对象是比较小的字符串，那么复杂度为 O(1) 。
+     * @param key
+     * @param value
+     * @param timeToLive
+     * @param unit
+     * @param replace
+     */
+    public void restoreByKey(String key, byte[] value, long timeToLive, TimeUnit unit, boolean replace) {
+        redisTemplate.restore(key, value, timeToLive,  unit,  replace);
+    }
+
+    /**
+     * 返回或保存给定列表、集合、有序集合 key 中经过排序的元素。排序默认以数字作为对象，值被解释为双精度浮点数，然后进行比较。
+     * @param query
+     * @return
+     */
+    public List<Object> sortByKey(SortQuery<String> query) {
+        return redisTemplate.sort(query);
+    }
+
+    /**
+     * 返回或保存给定列表、集合、有序集合 key 中经过排序的元素。排序默认以数字作为对象，值被解释为双精度浮点数，然后进行比较。
+     * @param query
+     * @param resultSerializer
+     * @param <T>
+     * @return
+     */
+    public <T> List<T> sortByKey(SortQuery<String> query, @Nullable RedisSerializer<T> resultSerializer) {
+        return redisTemplate.sort(query,resultSerializer);
+    }
+
+    /**
+     * 返回或保存给定列表、集合、有序集合 key 中经过排序的元素。排序默认以数字作为对象，值被解释为双精度浮点数，然后进行比较。
+     * @param query
+     * @param bulkMapper
+     * @param <T>
+     * @return
+     */
+    public <T> List<T> sortByKey(SortQuery<String> query, BulkMapper<T, Object> bulkMapper) {
+        return redisTemplate.sort(query,bulkMapper);
+    }
+
+    /**
+     * 返回或保存给定列表、集合、有序集合 key 中经过排序的元素。排序默认以数字作为对象，值被解释为双精度浮点数，然后进行比较。
+     * @param query
+     * @param bulkMapper
+     * @param resultSerializer
+     * @param <T>
+     * @param <S>
+     * @return
+     */
+    public <T, S> List<T> sortByKey(SortQuery<String> query, BulkMapper<T, S> bulkMapper,
+                               @Nullable RedisSerializer<S> resultSerializer) {
+        return redisTemplate.sort(query,bulkMapper,resultSerializer);
+    }
+
+    /**
+     * 返回或保存给定列表、集合、有序集合 key 中经过排序的元素。排序默认以数字作为对象，值被解释为双精度浮点数，然后进行比较。
+     * @param query
+     * @param storeKey
+     * @return
+     */
+    public Long sortByKey(SortQuery<String> query, String storeKey) {
+        return redisTemplate.sort(query,storeKey);
+    }
+
+    /**
+     * TTL key
+     * 功能描述：以秒为单位，返回给定 key 的剩余生存时间(TTL, time to live)。
+     *          在 Redis 2.8 以前，当 key 不存在，或者 key 没有设置剩余生存时间时，命令都返回 -1 。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：O(1)
+     * @param key 键 不能为null
+     * @return  当 key 不存在时，返回 -2 。当 key 存在但没有设置剩余生存时间时，返回 -1 。否则，以毫秒为单位，返回 key 的剩余生存时间。
+     */
+    public long ttlByKey(String key){
         return redisTemplate.getExpire(key,TimeUnit.SECONDS);
     }
 
     /**
-     * 判断key是否存在
-     * @param key 键
-     * @return true 存在 false不存在
+     * TYPE key
+     * 功能描述：返回 key 所储存的值的类型。
+     * 可用版本：>= 1.0.0
+     * 时间复杂度：O(1)
+     * @param key
+     * @return
+     *       1、none (key不存在)
+     *       2、string (字符串)
+     *      3、list (列表)
+     *      4、set (集合)
+     *      5、zset (有序集)
+     *      6、hash (哈希表)
      */
-    public boolean hasKey(String key){
-        try {
-            return redisTemplate.hasKey(key);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
+    public DataType typeByKey(String key) {
+        return redisTemplate.type(key);
     }
 
-    /**
-     * 删除缓存
-     * @param key 可以传一个值 或多个
-     */
-    @SuppressWarnings("unchecked")
-    public void del(String ... key){
-        if(key!=null&&key.length>0){
-            if(key.length==1){
-                redisTemplate.delete(key[0]);
-            }else{
-                redisTemplate.delete(CollectionUtils.arrayToList(key));
-            }
-        }
-    }
+    //SCAN暂未找到对应的方法
 
     //============================String=============================
     /**
@@ -198,7 +482,7 @@ public final class RedisUtil {
         try {
             redisTemplate.opsForHash().putAll(key, map);
             if(time>0){
-                expire(key, time);
+                expireByKey(key, time);
             }
             return true;
         } catch (Exception e) {
@@ -236,7 +520,7 @@ public final class RedisUtil {
         try {
             redisTemplate.opsForHash().put(key, item, value);
             if(time>0){
-                expire(key, time);
+                expireByKey(key, time);
             }
             return true;
         } catch (Exception e) {
@@ -341,7 +625,7 @@ public final class RedisUtil {
     public long sSetAndTime(String key,long time,Object...values) {
         try {
             Long count = redisTemplate.opsForSet().add(key, values);
-            if(time>0) expire(key, time);
+            if(time>0) expireByKey(key, time);
             return count;
         } catch (Exception e) {
             e.printStackTrace();
@@ -451,7 +735,7 @@ public final class RedisUtil {
     public boolean lSet(String key, Object value, long time) {
         try {
             redisTemplate.opsForList().rightPush(key, value);
-            if (time > 0) expire(key, time);
+            if (time > 0) expireByKey(key, time);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -485,7 +769,7 @@ public final class RedisUtil {
     public boolean lSet(String key, List<Object> value, long time) {
         try {
             redisTemplate.opsForList().rightPushAll(key, value);
-            if (time > 0) expire(key, time);
+            if (time > 0) expireByKey(key, time);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
--- src/test/java/com/geek/redis/sdk/config/RedisTest.java
+++ src/test/java/com/geek/redis/sdk/config/RedisTest.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: wanggang
@@ -21,34 +23,18 @@ public class RedisTest {
     private RedisTemplate redisTemplate;
 
     @Test
-    public void testString(){
-        //操作String类型的数据
-        ValueOperations<String, String> valueStr = redisTemplate.opsForValue();
-        //存储一条数据
-        valueStr.set("set:geek","天安门广场");
-//        //获取一条数据并输出
-        String goodsName = valueStr.get("set:geek");
-       System.out.println(goodsName);
+    public void testListDelete(){
+        List<String> test = new ArrayList<>();
+        test.add("1");
+        test.add("2");
+        test.add("3");
+        test.add("4");
 
-//        //存储多条数据
-//        Map<String,String> map = new HashMap<>();
-//        map.put("goodsName","福特汽车");
-//        map.put("goodsPrice","88888");
-//        map.put("goodsId","88");
-//
-//        valueStr.multiSet(map);
-//        //获取多条数据
-//        System.out.println("========================================");
-//        List<String> list = new ArrayList<>();
-//        list.add("goodsName");
-//        list.add("goodsPrice");
-//        list.add("goodsId");
-//        list.add("goodsProdu");
-//
-//        List<String> listKeys = valueStr.multiGet(list);
-//        for (String key : listKeys) {
-//            System.out.println(key);
-//        }
+        redisTemplate.opsForList().rightPushAll("test", test);
+        System.out.println(redisTemplate.opsForList().range("test", 0, -1)); // [1, 2, 3, 4]
+        redisTemplate.delete("test");
+        System.out.println(redisTemplate.opsForList().range("test", 0, -1)); // []
+
     }
 
 }
